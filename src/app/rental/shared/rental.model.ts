dd5d7a5f51271c30bc411b0af2fb380b1b4de661
Mahad Zakir Khan <mahadzakir@gmail.com>
2019-01-09 13:49:06 +0400

    Adds rental list, rental detail, rental service and rental model components

---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,17 @@
+export class Rental {
+
+	 id : string;
+	title : string;
+	city : string;
+	street : string;
+	category : string;
+	image : string;
+	description : string;
+	createdAt : string;
+	bedrooms : number;
+	dailyRate : number;
+	shared : boolean;
+
+   
+}
+
