07091deea565bbb21241ba76af16eb57fe86fbad
Mahad Zakir Khan <mahadzakir@gmail.com>
2019-01-14 11:26:10 +0400

    Adds map component and google API map

---
+++ src/app/common/map/map.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { AgmCoreModule } from '@agm/core';
+
+import { MapComponent } from './map.component';
+import {CamelizePipe} from 'ngx-pipes';
+import { MapService } from './map.service';
+
+@NgModule({
+  declarations: [
+MapComponent  
+],
+exports: [
+MapComponent
+],
+  imports: [
+      AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBHG47rFjETUYZDfQvm7ZYy1anNyGp6z7U'
+    })  
+  ],
+  providers: [
+  MapService,
+  CamelizePipe
+  ]
+})
+export class MapModule { }
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {NgPipesModule} from 'ngx-pipes';
-
+import {MapModule} from '../common/map/map.module';
 
 import { RentalComponent } from './rental.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
@@ -36,7 +36,8 @@ imports: [
 CommonModule,
 RouterModule.forChild(routes),
 HttpClientModule,
-NgPipesModule
+NgPipesModule,
+MapModule
 ],
 providers: [RentalService]
 })
